46a70b0af84bf6de9ef3de0e622236477ff4605c
kReign <alexscharles@gmail.com>
2016-03-20 21:27:37 -0500

    Err, it wasn't added before - HERE's the new title thing.

--- src/nick/dev/states/TitleState.java
+++ src/nick/dev/states/TitleState.java
@@ -1,10 +1,18 @@
 package nick.dev.states;
 
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
 
+import nick.dev.audio.AudioManager.Tracks;
 import nick.dev.base.Handler;
+import nick.dev.gfx.Assets;
+import nick.dev.gfx.SpriteSheet;
+import nick.dev.gfx.loadImage;
 import nick.dev.input.KeyManager.Keys;
+import nick.dev.maps.Map;
 import nick.dev.utilities.Utilities;
 
 /**
@@ -19,6 +27,9 @@ public class TitleState extends State {
 	private String[] optionList = new String[2];
 	private Runnable[] optionActions = new Runnable[2];
 	private Integer currentChoice = 0;
+	
+	private Integer highlightLength = 0;
+	private Integer highlightLengthMax = 100;
 	public Font f;
 
 	public TitleState(StateManager stateManager) {
@@ -46,31 +57,57 @@ public class TitleState extends State {
 //		}
 		
 		if (Handler.getKeyManager().keyIsPressed(Keys.ArrowDown)) {
+			
 			this.currentChoice = Math.abs((this.currentChoice + 1) % this.optionList.length);
+			this.highlightLength = 0;
+			Handler.getAudioManager().playSFX(Tracks.MenuChangeSFX);
+			
 		} else if (Handler.getKeyManager().keyIsPressed(Keys.ArrowUp)) {
+			
 			this.currentChoice = Math.abs((this.currentChoice - 1) % this.optionList.length);
+			this.highlightLength = 0;
+			Handler.getAudioManager().playSFX(Tracks.MenuChangeSFX);
 		}
 		
 		if (Handler.getKeyManager().keyIsPressed(Keys.Talk)) {
 			this.optionActions[this.currentChoice].run();
 		}
 		
+		if (this.highlightLength <= this.highlightLengthMax) {
+			this.highlightLength = Math.min(this.highlightLength + 12, this.highlightLengthMax);
+		}
+		
+		
 	}
 
 	@Override
 	public void render(Graphics g) {
 		g.setFont(f);
+		
 		// Clear Screen
-		g.clearRect(0, 0, Handler.getWidth(), Handler.getHeight());
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, Handler.getWidth(), Handler.getHeight());
 		
 		for (int i = 0; i < this.optionList.length; ++i) {
+			
+			// xPos = middle of the screen, yPos = the middle and then in a list.
 			int xPos = Handler.getWidth()/2 - g.getFontMetrics().stringWidth(this.optionList[0])/2;
 			int yPos = Handler.getHeight()/2 + (g.getFontMetrics().getHeight()+10)*i;
-			g.drawString(this.optionList[i], xPos, yPos);
 			
 			if (this.currentChoice.equals(i)) {
-				g.drawString(">", xPos - 30, yPos);
+				int fingerPosX = xPos - 64;
+				int fingerPosY = yPos - 30;
+				g.drawImage(Assets.finger, fingerPosX, fingerPosY, 50, 50, null);
+				
+				g.setColor(new Color(240, 240, 240));
+				
+				this.highlightLengthMax = g.getFontMetrics().stringWidth(this.optionList[0]);
+				
+				g.fillRect(xPos - 5, fingerPosY, this.highlightLength + 10, 40);
 			}
+			
+			g.setColor(Color.DARK_GRAY);
+			g.drawString(this.optionList[i], xPos, yPos);
 		}
 		
 		String title = Utilities.getPropValue("gameTitle");
