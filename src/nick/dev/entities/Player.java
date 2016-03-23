553567c26dc8a01787727f968b47f36d8a5b16f6
DartDivora <nsanft@sanft.com>
2016-03-22 20:00:41 -0500

    Movement now works in all directions
    Player movement now works in all combinations of directions (two keys at
    once).

--- src/nick/dev/entities/Player.java
+++ src/nick/dev/entities/Player.java
@@ -14,15 +14,16 @@ import nick.dev.maps.Map;
  * @version 1.1
  *****************************************************/
 public class Player extends Entity {
-	
+
 	/*****************************************************
 	 * Constructor. Sets position and initializes animations.
 	 *****************************************************/
 	public static Stats stats = new Stats();
+
 	public static Stats getStats() {
 		return Player.stats;
 	}
-	
+
 	private float moveSpeed = 3.5f;
 
 	/*****************************************************
@@ -30,78 +31,82 @@ public class Player extends Entity {
 	 *****************************************************/
 	public Player(float x, float y) {
 		super(x, y);
-		
+
 		this.name = "AlexSanft";
-		
+
 		// Initialize all of the animations.
 		this.animations[Direction.Up.getValue()] = new Animation(this.animSpeed, Assets.player_up);
 		this.animations[Direction.Right.getValue()] = new Animation(this.animSpeed, Assets.player_right);
 		this.animations[Direction.Down.getValue()] = new Animation(this.animSpeed, Assets.player_down);
 		this.animations[Direction.Left.getValue()] = new Animation(this.animSpeed, Assets.player_left);
-		
+
 		this.facingDirection = Direction.Down;
 	}
-	
+
 	/*****************************************************
-	 * Update called every frame. Handles movement and 
-	 * interaction with other entities.
+	 * Update called every frame. Handles movement and interaction with other
+	 * entities.
 	 *****************************************************/
 	@Override
 	public void update() {
 		super.update();
-		
+
 		this.doMovement();
 	}
-	
+
 	/*****************************************************
-	 * Handles the movement based on inputs for the player
-	 * character. Checks for collisions.
+	 * Handles the movement based on inputs for the player character. Checks for
+	 * collisions.
 	 *****************************************************/
 	private void doMovement() {
 		float newX = this.x;
 		float newY = this.y;
 		Integer xOffset = 0;
 		Integer yOffset = 0;
-		
+
 		// Get inputs and move based on them.
 		if (Handler.getKeyManager().keyIsDown(Keys.Up)) {
 			this.facingDirection = Direction.Up;
 			newY -= this.moveSpeed;
-			
-		} else if (Handler.getKeyManager().keyIsDown(Keys.Right)) {
+
+		}
+		if (Handler.getKeyManager().keyIsDown(Keys.Right)) {
 			this.facingDirection = Direction.Right;
 			newX += this.moveSpeed;
 			xOffset = 1;
-			
-		} else if (Handler.getKeyManager().keyIsDown(Keys.Down)) {
+
+		}
+		if (Handler.getKeyManager().keyIsDown(Keys.Down)) {
 			this.facingDirection = Direction.Down;
 			newY += this.moveSpeed;
 			yOffset = 1;
-			
-		} else if (Handler.getKeyManager().keyIsDown(Keys.Left)) {
+
+		}
+		if (Handler.getKeyManager().keyIsDown(Keys.Left)) {
 			this.facingDirection = Direction.Left;
 			newX -= this.moveSpeed;
 		}
-		
-		// Check if we are colliding. x and y offset are confusing, 
+
+		// Check if we are colliding. x and y offset are confusing,
 		// so I want to change it eventually.
 		if (!this.isColliding(newX, newY, xOffset, yOffset)) {
 			this.x = newX;
 			this.y = newY;
 		}
 	}
+
 	/*****************************************************
-	 * Check to see if the player is colliding with the map
-	 * or with any other entities.
+	 * Check to see if the player is colliding with the map or with any other
+	 * entities.
 	 *****************************************************/
 	private boolean isColliding(float newX, float newY, Integer xOff, Integer yOff) {
 		// Check for collision with the map
-		Integer destTileX = (int) Math.floor(newX  / Map.TileWidth) + xOff;
+		Integer destTileX = (int) Math.floor(newX / Map.TileWidth) + xOff;
 		Integer destTileY = (int) Math.floor(newY / Map.TileHeight) + yOff;
-		
+
 		boolean collidingWithMap = Handler.getWorld().tileIsSolid(destTileX, destTileY);
-		boolean collidingWithEntity = owner.isColliding(this, (int)newX, (int)newY);
-		
+		boolean collidingWithEntity = owner.isColliding(this, (int) newX, (int) newY);
+
 		return collidingWithMap || collidingWithEntity;
 	}
 }
