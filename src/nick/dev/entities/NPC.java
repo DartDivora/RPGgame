3c1bfc6df4c2d3a7dd31f0b56ac512fe450e17d2
DartDivora <nsanft@sanft.com>
2016-03-22 21:56:35 -0500

    NPC movement code
    Added some NPC movement code to check that they can and will move.

--- src/nick/dev/entities/NPC.java
+++ src/nick/dev/entities/NPC.java
@@ -6,8 +6,10 @@ import java.util.Map.Entry;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import nick.dev.base.Handler;
 import nick.dev.gfx.Animation;
 import nick.dev.gfx.Assets;
+import nick.dev.input.KeyManager.Keys;
 import nick.dev.utilities.Utilities;
 
 /**
@@ -17,65 +19,103 @@ import nick.dev.utilities.Utilities;
  * @version 1.1
  */
 public class NPC extends Entity {
-	
+
+	private float moveSpeed = 3.5f;
+
 	/**************************************************************
-	 * Stores the data for the NPCs in the game. Works the same as
-	 * the Monster container.
+	 * Stores the data for the NPCs in the game. Works the same as the Monster
+	 * container.
 	 **************************************************************/
 	private static HashMap<String, NPC> npcData;
 	static {
 		Gson gson = new Gson();
 		String JSONString = Utilities.getStringFromFile(Utilities.getPropValue("npcJSON"));
-		
-		npcData = gson.fromJson(JSONString, new TypeToken<HashMap<String, NPC>>(){}.getType());
-		
+
+		npcData = gson.fromJson(JSONString, new TypeToken<HashMap<String, NPC>>() {
+		}.getType());
+
 		for (Entry<String, NPC> entry : npcData.entrySet()) {
 			entry.getValue().initialize();
 		}
 	}
-	
+
 	public static HashMap<String, NPC> getNPCData() {
 		return npcData;
 	}
+
 	/*************************************************************/
 
 	private Integer dialogueID;
 
 	/*****************************************************
-	 * Constructor. Probably won't do anything since we're 
-	 * using GSON.
+	 * Constructor. Probably won't do anything since we're using GSON.
 	 *****************************************************/
 	public NPC(float x, float y) {
 		super(x, y);
 	}
-	
+
 	/*****************************************************
-	 * Called when gson creates the instances from the 
-	 * json file.
+	 * Handles the movement for NPCs. Checks for collisions.
+	 *****************************************************/
+	private void doMovement() {
+		Integer xOffset = 0;
+		Integer yOffset = 0;
+
+		// Get inputs and move based on them.
+		if (Handler.getKeyManager().keyIsDown(Keys.Up)) {
+			this.facingDirection = Direction.Up;
+			this.y -= this.moveSpeed;
+
+		}
+		if (Handler.getKeyManager().keyIsDown(Keys.Right)) {
+			this.facingDirection = Direction.Right;
+			this.x += this.moveSpeed;
+			xOffset = 1;
+
+		}
+		if (Handler.getKeyManager().keyIsDown(Keys.Down)) {
+			this.facingDirection = Direction.Down;
+			this.y += this.moveSpeed;
+			yOffset = 1;
+
+		}
+		if (Handler.getKeyManager().keyIsDown(Keys.Left)) {
+			this.facingDirection = Direction.Left;
+			this.x -= this.moveSpeed;
+		}
+
+		// Check if we are colliding. x and y offset are confusing,
+		// so I want to change it eventually.
+		this.resolveCollisions(this.x, this.y, xOffset, yOffset);
+	}
+
+	/*****************************************************
+	 * Called when gson creates the instances from the json file.
 	 *****************************************************/
 	public void initialize() {
-		
+
 		this.animSpeed = 30;
-		
+
 		// Initialize all of the animations.
 		this.animations = new Animation[4];
 		this.animations[Direction.Up.getValue()] = new Animation(this.animSpeed, Assets.DEFAULT_ANIM_UP);
 		this.animations[Direction.Right.getValue()] = new Animation(this.animSpeed, Assets.DEFAULT_ANIM_RIGHT);
 		this.animations[Direction.Down.getValue()] = new Animation(this.animSpeed, Assets.DEFAULT_ANIM_DOWN);
 		this.animations[Direction.Left.getValue()] = new Animation(this.animSpeed, Assets.DEFAULT_ANIM_LEFT);
-		
+
 		this.facingDirection = Direction.Down;
 	}
-	
+
 	/*****************************************************
-	 * Update called every frame. Just calls super's update,
-	 * which updates the animation.
+	 * Update called every frame. Just calls super's update, which updates the
+	 * animation.
 	 *****************************************************/
 	@Override
 	public void update() {
 		super.update();
+		this.doMovement();
 	}
-	
+
 	/*****************************************************
 	 * Initiate dialog state with dialogID as the argument.
 	 *****************************************************/
@@ -83,7 +123,7 @@ public class NPC extends Entity {
 	public void onInteract() {
 		System.out.println(dialogueID);
 	}
-	
+
 	public Integer getDialogueID() {
 		return this.dialogueID;
 	}
