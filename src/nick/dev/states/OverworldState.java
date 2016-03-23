0f430930a7c04e7285d46da6ac3ee1accbecac32
kReign <alexscharles@gmail.com>
2016-03-23 16:33:17 -0500

    Current music now does not stop when leaving overworld state (for dialog state, etc)

--- src/nick/dev/states/OverworldState.java
+++ src/nick/dev/states/OverworldState.java
@@ -65,7 +65,6 @@ public class OverworldState extends State {
 	 *****************************************************/
 	@Override
 	public void onExit() {
-		Handler.getAudioManager().pauseCurrentTrack();
 	}
 
 	/*****************************************************
