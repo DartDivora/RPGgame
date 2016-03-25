8bbc3d30e127a5a95cd63a12bc178cd09386d26f
DartDivora <nsanft@sanft.com>
2016-03-24 21:58:14 -0500

    Additional changes to DisplayJavaFX to show mouse action examples.
    Basically that,

--- src/nick/dev/display/DisplayJavaFX.java
+++ src/nick/dev/display/DisplayJavaFX.java
@@ -1,23 +1,50 @@
 package nick.dev.display;
 
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+import nick.dev.utilities.Utilities;
 
 public class DisplayJavaFX extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-		primaryStage.setTitle("This is a test");
+		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+		Integer width = gd.getDisplayMode().getWidth();
+		Integer height = gd.getDisplayMode().getHeight();
+
+		Utilities.Debug(width);
+		Utilities.Debug(height);
+		width = Integer.parseInt(Utilities.getPropValue("gameWidth"));
+		height = Integer.parseInt(Utilities.getPropValue("gameHeight"));
+		primaryStage.setTitle(Utilities.getPropValue("gameTitle"));
 		Group g = new Group();
-		Canvas canvas = new Canvas(1000, 1000);
+		Canvas canvas = new Canvas(width, height);
 		GraphicsContext gc = canvas.getGraphicsContext2D();
 		render(gc);
 		g.getChildren().add(canvas);
-		primaryStage.setScene(new Scene(g));
+		Scene s = new Scene(g);
+
+		// Apparently this is how MouseListeners work in JavaFX...
+		s.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				Circle circle = new Circle(event.getSceneX(), event.getSceneY(), 30);
+				circle.setFill(Color.YELLOW);
+				g.getChildren().add(circle);
+			}
+		});
+		primaryStage.setScene(s);
 		primaryStage.show();
 	}
 
