8aba99c76447c25b86e108c3dcadd7a74b87f4b4
DartDivora <nsanft@sanft.com>
2016-03-27 15:12:38 -0500

    Added Armor and Weapon classes
    Just added some basic Armor and Weapon classes. We will discuss how to
    implement them in a logical way later...

---
+++ src/nick/dev/items/Armor.java
@@ -0,0 +1,17 @@
+package nick.dev.items;
+
+/**
+ * This class handles all armor equippable by the player.
+ * 
+ * @author acharles, nsanft
+ *
+ */
+public class Armor extends Item {
+
+	@Override
+	public void initialize() {
+		// Might need this sometime...
+		super.initialize();
+	}
+
+}
---
+++ src/nick/dev/items/Weapon.java
@@ -0,0 +1,17 @@
+package nick.dev.items;
+
+/**
+ * This class handles all weapons equippable by the player.
+ * 
+ * @author acharles, nsanft
+ *
+ */
+public class Weapon extends Item {
+
+	@Override
+	public void initialize() {
+		// Might need this sometime...
+		super.initialize();
+	}
+
+}
